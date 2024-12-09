417fb7faec5e1cee42f356efcb72e14c115b22fc
Sylvain Schmitt <sylvain.m.schmitt@gmail.com>
2024-12-09 12:07:56 +0100

    environment 0.1.0.9013: paracou and bafog soil fixed using bb, #1

--- scripts/get_soil.py
+++ scripts/get_soil.py
@@ -9,8 +9,7 @@ out_file = snakemake.output[0]
 
 # test
 # sites_tab = "config/sites.tsv"
-# site = "Uppangala_LP2"
-# out_file = "data/soil/Uppangala_LP2_soil.tsv"
+# site = "Paracou_1"
 
 # libs
 import pandas as pd
@@ -21,12 +20,14 @@ sites = pd.read_table(sites_tab)
 sites['site_plot'] = sites['site'] + "_" + sites['plot']
 sites = sites[sites["site_plot"]==site]
 ee.Initialize(opt_url='https://earthengine-highvolume.googleapis.com')
-leg = ee.Geometry.Rectangle(sites["longitude"].values[0], sites["latitude"].values[0], sites["longitude"].values[0], sites["latitude"].values[0])
+radius = 0.001
+leg = ee.Geometry.Rectangle(sites["longitude"].values[0]-radius, sites["latitude"].values[0]-radius, 
+                            sites["longitude"].values[0]+radius, sites["latitude"].values[0]+radius)
 
 def get_var(var):
     im = ee.Image("projects/soilgrids-isric/" + var)
     ic = ee.ImageCollection.fromImages([im])                       
-    ds = xr.open_mfdataset([ic], engine='ee', geometry=leg)
+    ds = xr.open_mfdataset([ic], engine='ee',  projection=ic.first().select(0).projection(), geometry=leg)
     return ds.to_dataframe()
 
 all_tabs = [get_var("bdod_mean"),
